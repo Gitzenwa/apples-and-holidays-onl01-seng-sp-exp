661e5cde8d7e8be1fe45c97ba5ebab9ed13be4d7
Ezenwa Onukwue <eonukwue@gmail.com>
2020-09-27 01:04:17 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -67,7 +67,7 @@ def all_supplies_in_holidays(holiday_hash)
   holiday_hash.each do |season, holiday |
     puts "#{season.to_s.capitalize!}:"
     holiday.each do |holiday, supplies|
-    puts "  #{holiday}"
+    puts "  #{holiday}: supplies.join(", ")"
   end
 
 end
