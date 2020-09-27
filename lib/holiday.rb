5d66e4e795f33a38b1fd1ff4dda55dfb12c84b27
Ezenwa Onukwue <eonukwue@gmail.com>
2020-09-27 01:18:28 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -74,6 +74,7 @@ end
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
   # include the string "BBQ"
+  holiday_array = []
   holiday_hash.each do |season, holiday|
     holiday.each do |holiday, supplies|
       if supplies.include?("BBQ")
