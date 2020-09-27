7afb9a9de9f0374fa9ace50ee4ac990efb32e7c9
Ezenwa Onukwue <eonukwue@gmail.com>
2020-09-27 00:24:22 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -29,9 +29,8 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
 
     holiday_hash.each do | season, holiday |
         if season == :winter
-
-          holiday << supply
-          binding.pry
+            season.each do |hday, supplies|
+              supplies << supply
         end
       end
 
