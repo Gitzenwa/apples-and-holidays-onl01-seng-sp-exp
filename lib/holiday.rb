46035bd06b415cd2ecc839febfe330a9e5dffa6e
Ezenwa Onukwue <eonukwue@gmail.com>
2020-09-27 00:48:08 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -49,7 +49,8 @@ end
 
 def all_winter_holiday_supplies(holiday_hash)
   # return an array of all of the supplies that are used in the winter season
-  holiday_hash[:supplies]
+  xmas = holiday_hash[:winter][:christmas]
+  ny = holiday_hash[:winter][:new_years]
 
 end
 
