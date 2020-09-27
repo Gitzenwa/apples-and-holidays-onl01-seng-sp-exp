aafc16d64eb22eb9df1c7aba5d30ddfc8148d7ba
Ezenwa Onukwue <eonukwue@gmail.com>
2020-09-27 00:56:41 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, holiday |
-    puts holiday.to_s
+    puts season.to_s
   end
 
 end
